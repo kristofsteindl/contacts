35c7b1e737d40724823a13af55969dde4a392daf
Kristóf Steindl <ksteindl@chemaxon.com>
2021-10-05 17:34:38 +0200

    created Contact Entity

---
+++ src/main/java/com/ksteindl/contacts/domain/Status.java
@@ -0,0 +1,5 @@
+package com.ksteindl.contacts.domain;
+
+public enum Status {
+    ACTIVE, DELETED;
+}
---
+++ src/main/java/com/ksteindl/contacts/domain/entities/Contact.java
@@ -0,0 +1,129 @@
+package com.ksteindl.contacts.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ksteindl.contacts.domain.Status;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+public class Contact {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    @OneToOne
+    private Company company;
+
+    @Lob
+    private String comment;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @JsonIgnore
+    private OffsetDateTime createdAt;
+
+    @JsonIgnore
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
