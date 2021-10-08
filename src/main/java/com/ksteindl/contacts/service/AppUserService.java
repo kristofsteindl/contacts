eac26a923bf4b87f5756361e7556903e70ed5011
Kristóf Steindl <ksteindl@chemaxon.com>
2021-10-08 19:42:50 +0200

    finished login

--- src/main/java/com/ksteindl/contacts/service/AppUserService.java
+++ src/main/java/com/ksteindl/contacts/service/AppUserService.java
@@ -5,6 +5,7 @@ import com.ksteindl.contacts.domain.entities.AppUser;
 import com.ksteindl.contacts.web.input.AppUserInput;
 import com.ksteindl.contacts.domain.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +14,14 @@ public class AppUserService {
     @Autowired
     private AppUserRepository appUserRepository;
 
-//    @Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public AppUser saveUser(AppUserInput userInput) {
         AppUser appUser = new AppUser();
         appUser.setUsername(userInput.getUsername());
-        appUser.setPassword(userInput.getPassword());
+        String encodedPassword = bCryptPasswordEncoder.encode(userInput.getPassword());
+        appUser.setPassword(encodedPassword);
         return appUserRepository.save(appUser);
     }
 
--- src/main/java/com/ksteindl/contacts/web/controller/AppUserController.java
+++ src/main/java/com/ksteindl/contacts/web/controller/AppUserController.java
@@ -1,11 +1,19 @@
 package com.ksteindl.contacts.web.controller;
 
 import com.ksteindl.contacts.domain.entities.AppUser;
+import com.ksteindl.contacts.security.JwtAuthenticationFilter;
+import com.ksteindl.contacts.security.JwtProvider;
+import com.ksteindl.contacts.security.payload.JwtLoginResponse;
+import com.ksteindl.contacts.security.payload.LoginRequest;
 import com.ksteindl.contacts.web.WebUtils;
 import com.ksteindl.contacts.web.input.AppUserInput;
 import com.ksteindl.contacts.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +29,33 @@ public class AppUserController {
     @Autowired
     private AppUserService appUserService;
 
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 
     @PostMapping("/register")
     public ResponseEntity<AppUser> registerUser(@Valid @RequestBody AppUserInput userInput, BindingResult result) {
         WebUtils.throwExceptionIfNotValid(result);
         AppUser newUser = appUserService.saveUser(userInput);
         return ResponseEntity.ok(newUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtLoginResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        WebUtils.throwExceptionIfNotValid(result);
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = JwtAuthenticationFilter.TOKEN_PREFIX + jwtProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtLoginResponse(true, jwt));
+
     }
 
 }
